548148dc1a186fd3fe9eeea4beffdd1c06ae7144
AIT-MOHAMED Oussama <sadosami297@gmail.com>
2025-04-01 11:12:09 +0000

    First Commit

---
+++ database/migrations/2025_03_30_211512_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('entreprise');
+            $table->integer('note_personnelle');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
